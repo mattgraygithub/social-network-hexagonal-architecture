cd03a5e967130fcd08b100739d99aa5ec3160e01
Matt Gray <mattgray83@hotmail.co.uk>
2021-04-23 15:43:28 +0100

    Refactoring

--- src/main/java/com/mattgray/socialnetworkkata/SocialNetwork.java
+++ src/main/java/com/mattgray/socialnetworkkata/SocialNetwork.java
@@ -1,9 +1,14 @@
 package com.mattgray.socialnetworkkata;
 
+import com.mattgray.socialnetworkkata.clock.ClockService;
 import com.mattgray.socialnetworkkata.clock.ClockServiceImpl;
+import com.mattgray.socialnetworkkata.following.WallService;
 import com.mattgray.socialnetworkkata.following.WallServiceImpl;
+import com.mattgray.socialnetworkkata.posting.TimelineService;
 import com.mattgray.socialnetworkkata.posting.TimelineServiceImpl;
 import com.mattgray.socialnetworkkata.users.InMemoryUserRepository;
+import com.mattgray.socialnetworkkata.users.User;
+import com.mattgray.socialnetworkkata.users.UserRepository;
 import com.mattgray.socialnetworkkata.users.UserService;
 
 import java.time.Clock;
@@ -12,6 +17,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SocialNetwork {
+
+    private static final ArrayList<User> users = new ArrayList<>();
+    private static final UserRepository userRepository = new InMemoryUserRepository(users);
+    private static final ClockService clockService = new ClockServiceImpl();
+    private static final TimelineService timelineService = new TimelineServiceImpl(clockService);
+    private static final WallService wallService = new WallServiceImpl(clockService);
+    private static final UserService userService = new UserService(userRepository, timelineService, wallService);
 
     private final CommandProcessor commandProcessor;
     private final Clock clock;
@@ -24,7 +36,7 @@ public class SocialNetwork {
     public static void main(String[] args) {
         System.out.println("Welcome to the Social Network. Please enter a command");
 
-        SocialNetwork socialNetwork = new SocialNetwork(new CommandProcessor(new UserService(new InMemoryUserRepository(new ArrayList<>()), new TimelineServiceImpl(new ClockServiceImpl()), new WallServiceImpl(new ClockServiceImpl()))), Clock.systemDefaultZone());
+        SocialNetwork socialNetwork = new SocialNetwork(new CommandProcessor(userService), Clock.systemDefaultZone());
 
         socialNetwork.run();
     }
